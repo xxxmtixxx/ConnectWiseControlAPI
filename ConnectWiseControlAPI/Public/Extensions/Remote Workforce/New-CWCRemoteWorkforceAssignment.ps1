eb28f1e42adaf8cc82781b311365608659f0d252
xxxmtixxx <clemons.paul.l@gmail.com>
2024-01-06 17:58:51 -0600

    Add files via upload

--- ConnectWiseControlAPI/Public/Extensions/Remote Workforce/New-CWCRemoteWorkforceAssignment.ps1
+++ ConnectWiseControlAPI/Public/Extensions/Remote Workforce/New-CWCRemoteWorkforceAssignment.ps1
@@ -1,18 +1,45 @@
+function ConvertTo-CustomJson {
+    param(
+        [Parameter(Mandatory=$True)]
+        [array]$Array
+    )
+
+    $JsonArray = @()
+    foreach ($Item in $Array) {
+        if ($Item -is [array]) {
+            $JsonArray += ('["' + ($Item -join '","') + '"]')
+        } else {
+            $JsonArray += ('"' + $Item + '"')
+        }
+    }
+    return ('[' + ($JsonArray -join ',') + ']')
+}
+
 function New-CWCRemoteWorkforceAssignment {
     [CmdletBinding(SupportsShouldProcess)]
     param(
         [Parameter(Mandatory=$True)]
         [guid]$SessionID,
+        [string]$UserName,
         [string]$DisplayName
     )
 
+    #Write-Host ""
+    #Write-Host "SessionID: $SessionID"
+    #Write-Host "UserName: $UserName"
+    #Write-Host "DisplayName: $DisplayName"
+
     $Endpoint = 'App_Extensions/2c4f522f-b39a-413a-8807-dc52a2fce13e/Service.ashx/AddAssignmentNoteToSession'
 
-    $Body = ConvertTo-Json @(
-        $GUID,
-        "UserName:$($UserName),UserDisplayName:$($DisplayName)"
+    # Create the body as an array of arrays and strings
+    $Body = ConvertTo-CustomJson @(
+        @("All Machines by Company"),
+        @($SessionID.ToString()),
+        "UserName:$UserName,UserDisplayName:$DisplayName"
     )
-    Write-Verbose $Body
+
+    #Write-Host ""
+    #Write-Host "Body: $Body"
 
     $WebRequestArguments = @{
         Endpoint = $Endpoint
