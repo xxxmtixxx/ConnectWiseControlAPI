17a67094b424d35f24f7a4598a47d6204e774367
Pablo Clemons <pclemons@raritysolutions.com>
2024-12-19 22:33:12 -0600

    Added Get-CWCUser

---
+++ ConnectWiseControlAPI/Public/SecurityService/Get-CWCUser.ps1
@@ -0,0 +1,32 @@
+function Get-CWCUser {
+    [CmdletBinding()]
+    param(
+        [Parameter(Mandatory=$True)]
+        [string]$User
+    )
+
+    try {
+        Write-Host "Retrieving security configuration info..."
+        $Security = Get-CWCSecurityConfigurationInfo -ErrorAction Stop
+        $Internal = $Security.UserSources | Where-Object { $_.ResourceKey -eq $script:InternalUserSource }
+        
+        if (-not $Internal) {
+            Write-Error "Unable to find the internal user source."
+            return $null
+        }
+
+        Write-Host "Searching for user in internal source..."
+        $UserMatch = $Internal.Users | Where-Object { $_.Name -eq $User -or $_.Email -eq $User }
+        
+        if ($UserMatch) {
+            Write-Host "Successfully retrieved user: $User"
+            return $UserMatch
+        } else {
+            Write-Error "User not found: $User"
+            return $null
+        }
+    } catch {
+        Write-Error "Error retrieving user: $_"
+        return $null
+    }
+}
